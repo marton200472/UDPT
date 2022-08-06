99148d352028d4fc39c69838f1186e7d1b689b5f
marton200472 <marton200472@gmail.com>
2022-08-06 14:32:15 +0200

    added mvc webinterface

---
+++ WebInterface/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebInterface.Models;
+using Shared;
+using Shared.Models;
+
+namespace WebInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TrackerDbContext _trackerDbContext;
+
+        public HomeController(ILogger<HomeController> logger, TrackerDbContext dbContext)
+        {
+            _logger = logger;
+            _trackerDbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TrackerData()
+        {
+            return PartialView(new TrackerDataViewModel(_trackerDbContext.Torrents.ToList(), _trackerDbContext.Peers.ToList()));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebInterface/Models/TrackerDataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+
+namespace WebInterface.Models
+{
+    public class TrackerDataViewModel
+    {
+        public TrackerDataViewModel(List<Torrent> torrents, List<Peer> peers)
+        {
+            Torrents = torrents;
+            Peers = peers;
+        }
+
+        public List<Torrent> Torrents { get; set; }
+        public List<Peer> Peers { get; set; }
+    }
+}
